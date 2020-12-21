81474dd8bd305002d08a0aecd81d2db3134c0dae
Nick Thijssen <nickthijssen1994@gmail.com>
2020-12-21 15:20:47 +0100

    Dissabled Identity

--- KillerAppASP/KillerAppASP/Startup.cs
+++ KillerAppASP/KillerAppASP/Startup.cs
@@ -23,11 +23,11 @@ namespace KillerAppASP
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddDbContext<ApplicationDbContext>(options =>
-			  options.UseMySql(
-				  Configuration.GetConnectionString("DefaultConnection")));
-			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-				.AddEntityFrameworkStores<ApplicationDbContext>();
+			//services.AddDbContext<ApplicationDbContext>(options =>
+			//  options.UseMySql(
+			//	  Configuration.GetConnectionString("DefaultConnection")));
+			//services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+			//	.AddEntityFrameworkStores<ApplicationDbContext>();
 			services.AddControllersWithViews();
 			services.AddRazorPages();
 
